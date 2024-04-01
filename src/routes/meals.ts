8006e01b6e028d14723222eebeb7943803158bfa
Diego Fontenelle <diegofontenelle@Diegos-MacBook-Pro.local>
2024-04-01 19:10:24 -0300

    feat: list user meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,8 +2,17 @@ import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const meals = await knex('meals').select().where('session_id', sessionId)
+
+    return { meals }
+  })
+
   app.post('/', async (request, reply) => {
     const mealSchema = z.object({
       name: z.string(),
