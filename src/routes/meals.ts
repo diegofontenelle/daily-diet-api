31493c2314cb46d865d63e6213af9ddb382a0eea
Diego Fontenelle <diegofontenelle@Diegos-MacBook-Pro.local>
2024-04-01 23:49:17 -0300

    feat: update meal

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -58,6 +58,44 @@ export async function mealsRoutes(app: FastifyInstance) {
     },
   )
 
+  app.put(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const mealParamsSchema = z.object({
+        id: z.string(),
+      })
+
+      const mealBodySchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        isCheatMeal: z.boolean().optional(),
+        createdAt: z.string().optional(),
+      })
+
+      const { id } = mealParamsSchema.parse(request.params)
+      const { name, description, isCheatMeal, createdAt } =
+        mealBodySchema.parse(request.body)
+
+      const { sessionId } = request.cookies
+
+      const meal = await knex('meals')
+        .update({
+          name,
+          description,
+          is_cheat_meal: isCheatMeal,
+          created_at: createdAt,
+        })
+        .where({
+          id,
+          session_id: sessionId,
+        })
+        .returning('*')
+
+      return reply.status(200).send({ meal })
+    },
+  )
+
   app.post('/', async (request, reply) => {
     const mealSchema = z.object({
       name: z.string(),
